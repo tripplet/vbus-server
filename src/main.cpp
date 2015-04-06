ee429f3ca4eef1ba30e8a3ccbad774e61fa47bd5
Tobias Tangemann <tobias@tangemann.org>
2015-04-06 20:50:37 +0200

    Fix some paths

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include <json/json.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 
-#define DB_PATH "/tmp/vbus-data.db"
+#define DB_PATH "/srv/http/vbus-data.db"
 
 int main(int argc, char const *argv[])
 {
   try {
     SQLite::Database db(DB_PATH);
 
-    SQLite::Statement query(db, "SELECT datetime(time, 'localtime'),temp1,temp2,temp3,temp4,pump1,pump2 FROM data ORDER BY id LIMIT 1;");
+    SQLite::Statement query(db, "SELECT datetime(time, 'localtime'),temp1,temp2,temp3,temp4,pump1,pump2 FROM data ORDER BY id DESC LIMIT 1;");
     if (query.executeStep()) {
         std::cout << query.getColumn(0).getText() << std::endl;
     }
