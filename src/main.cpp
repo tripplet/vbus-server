f64f927a4a2bccdfeb434dee4702697da9198eb2
Tobias Tangemann <tobias@tangemann.org>
2015-04-11 14:32:01 +0200

    Read query string

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <string>
 #include <SQLiteCpp/SQLiteCpp.h>
 
-#define DB_PATH "/srv/http/vbus-data.db"
+#define DB_PATH "vbus-data.db"
 
 int main(int argc, char const *argv[])
 {
@@ -10,12 +11,22 @@ int main(int argc, char const *argv[])
   try {
     SQLite::Database db(DB_PATH);
 
+    std::string time_query = std::string("-1 hour");
+
+    char *cgi_query = getenv("QUERY_STRING");
+
+    if (cgi_query) {
+      time_query = std::string(cgi_query);
+    }
+
     SQLite::Statement query(db, "SELECT "
-                                 "datetime(time, 'localtime') as time, "
-                                 "temp1, temp2, temp3, temp4, pump1, pump2 "
-                                 "FROM data "
-                                 "WHERE time > (SELECT DATETIME('now', '-2 hour')) "
-                                 "ORDER BY id DESC");
+                                "datetime(time, 'localtime') as time, "
+                                "temp1, temp2, temp3, temp4, pump1, pump2 "
+                                "FROM data "
+                                "WHERE time > (SELECT DATETIME('now', ?)) "
+                                "ORDER BY id DESC");
+
+    query.bind(1, time_query);
 
     std::cout << "{\"data\":[";
 
@@ -30,13 +41,13 @@ int main(int argc, char const *argv[])
       }
 
       std::cout << "["
-                << "\"" << query.getColumn(0).getText() << "\","
-                << query.getColumn(1).getDouble() << ","
-                << query.getColumn(2).getDouble() << ","
-                << query.getColumn(3).getDouble() << ","
-                << query.getColumn(4).getDouble() << ","
-                << query.getColumn(5).getInt()    << ","
-                << query.getColumn(6).getInt()    << "]";
+                << "\"" << query.getColumn(0).getText()   << "\","
+                        << query.getColumn(1).getDouble() << ","
+                        << query.getColumn(2).getDouble() << ","
+                        << query.getColumn(3).getDouble() << ","
+                        << query.getColumn(4).getDouble() << ","
+                        << query.getColumn(5).getInt()    << ","
+                        << query.getColumn(6).getInt()    << "]";
     }
 
     std::cout << "]}";
