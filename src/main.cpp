6801768ebbd28bd715d180c769307b3f8bc3a554
Tobias Tangemann <tobias@tangemann.org>
2015-04-12 21:20:55 +0200

    Added website and modified server to send csv data

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
+#include <algorithm>
 #include <SQLiteCpp/SQLiteCpp.h>
 
-#define DB_PATH "vbus-data.db"
+#define DB_PATH "/srv/http/vbus-data.db"
+
+void find_and_replace(std::string& source, std::string const& find, std::string const& replace)
+{
+  for(std::string::size_type i = 0; (i = source.find(find, i)) != std::string::npos;)
+  {
+    source.replace(i, find.length(), replace);
+    i += replace.length();
+  }
+}
+
 
 int main(int argc, char const *argv[])
 {
   std::cout << "Content-type:text/comma-separated-values\r\n"
-            << "Access-Control-Allow-Origin: *'\r\n\r\n";
+            << "Access-Control-Allow-Origin: *\r\n\r\n";
 
   try {
     SQLite::Database db(DB_PATH);
 
     std::string time_query = std::string("-1 hour");
 
-    char *cgi_query = getenv("QUERY_STRING");
+    char *cgi_query = std::getenv("QUERY_STRING");
 
     if (cgi_query) {
       time_query = std::string(cgi_query);
+      find_and_replace(time_query, "%20", " ");
     }
 
     SQLite::Statement query(db, "SELECT "
@@ -25,20 +38,22 @@ int main(int argc, char const *argv[])
                                 "temp1, temp2, temp3, temp4, pump1, pump2 "
                                 "FROM data "
                                 "WHERE time > (SELECT DATETIME('now', ?)) "
-                                "ORDER BY id DESC");
+                                "ORDER BY id");
 
     query.bind(1, time_query);
 
-    std::cout << "Datum,Ofen,Speicher (unten),Speicher (oben), Heizung, Ventil-Ofen, Ventil-Heizung" << std::endl;
+    //std::cout << time_query<< std::endl;
+
+    std::cout << "Datum,Ofen,Speicher-unten,Speicher-oben,Heizung,Ventil-Ofen,Ventil-Heizung" << std::endl;
 
     while (query.executeStep()) {
-      std::cout << "\"" << query.getColumn(0).getText()   << "\","
-                        << query.getColumn(1).getDouble() << ","
-                        << query.getColumn(2).getDouble() << ","
-                        << query.getColumn(3).getDouble() << ","
-                        << query.getColumn(4).getDouble() << ","
-                        << query.getColumn(5).getInt()    << ","
-                        << query.getColumn(6).getInt()    << std::endl;
+      std::cout << query.getColumn(0).getText()   << ","
+                << query.getColumn(1).getDouble() << ","
+                << query.getColumn(2).getDouble() << ","
+                << query.getColumn(3).getDouble() << ","
+                << query.getColumn(4).getDouble() << ","
+                << query.getColumn(5).getInt()    << ","
+                << query.getColumn(6).getInt()    << std::endl;
     }
   }
   catch (std::exception& e) {
