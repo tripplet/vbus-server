4d5855542c135f389f934bb89defcc7dbba44291
Tobias Tangemann <tobiastangemann@gmail.com>
2015-11-01 18:44:47 +0100

    Progress on json

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@ typedef std::unordered_map<std::string, std::string> parameterMap;
 
 parameterMap* parseURL(const std::string& url);
 
+void printCsvResult(SQLite::Statement& query, std::ostream& stream, bool isFirstRow);
+void printJsonResult(SQLite::Statement& query, std::ostream& stream, bool isFirstRow);
+
 void find_and_replace(std::string& source, std::string const& find, std::string const& replace)
 {
   for(std::string::size_type i = 0; (i = source.find(find, i)) != std::string::npos;)
@@ -33,7 +36,7 @@ int main(int argc, char const *argv[])
 {
   // Set decimal seperator to '.'
   std::cout.imbue(std::locale(std::cout.getloc(), new punct_facet()));
-  
+
   // Print HTTP header
   std::cout << "Content-type: text/comma-separated-values\r\n"
             << "Cache-Control: no-cache, no-store, must-revalidate\r\n"
@@ -42,7 +45,7 @@ int main(int argc, char const *argv[])
             << "Access-Control-Allow-Origin: *\r\n\r\n";
 
   std::unique_ptr<parameterMap> requestParameter;
-    
+
   try
   {
     SQLite::Database db(DB_PATH);
@@ -56,12 +59,10 @@ int main(int argc, char const *argv[])
     else {
       requestParameter.reset(new parameterMap());
     }
-      
 
-    if (requestParameter->count("timespan") == 0)
-    {
-      (*requestParameter)["timespan"] = "-1 hour";
-    }
+    // Set default parameter
+    if (requestParameter->count("timespan") == 0) { (*requestParameter)["timespan"] = "-1 hour"; }
+    if (requestParameter->count("format") == 0)   { (*requestParameter)["format"] = "csv"; }
 
     SQLite::Statement query(db, "SELECT "
                                 "datetime(time, 'localtime') as time, "
@@ -72,26 +73,72 @@ int main(int argc, char const *argv[])
 
     query.bind(1, (*requestParameter)["timespan"]);
 
-    std::cout << "Datum,Ofen,Speicher-unten,Speicher-oben,Heizung,Ventil-Ofen,Ventil-Heizung" << std::endl;
 
+    if ((*requestParameter)["format"] == "csv")
+    {
+      std::cout << "Datum,Ofen,Speicher-unten,Speicher-oben,Heizung,Ventil-Ofen,Ventil-Heizung" << std::endl;
+    }
+    else if ((*requestParameter)["format"] == "json")
+    {
+      std::cout << "{ \"data\": [" << std::endl;
+    }
+
+    bool isFirstRow = true;
+    
     while (query.executeStep())
     {
-      std::cout << query.getColumn(0).getText()   << ","
-                << query.getColumn(1).getDouble() << ","
-                << query.getColumn(2).getDouble() << ","
-                << query.getColumn(3).getDouble() << ","
-                << query.getColumn(4).getDouble() << ","
-                << query.getColumn(5).getInt()    << ","
-                << query.getColumn(6).getInt()    << std::endl;
+      if ((*requestParameter)["format"] == "csv")
+      {
+        printCsvResult(query, std::cout, isFirstRow);
+      }
+      else if ((*requestParameter)["format"] == "json")
+      {
+        printJsonResult(query, std::cout, isFirstRow);
+      }
+      
+      isFirstRow = false;
     }
+
+    if ((*requestParameter)["format"] == "json")
+    {
+      std::cout << std::endl<< "] }" << std::endl;
+    }
+    
   }
   catch (std::exception& e)
   {
       std::cerr << e.what() << std::endl;
       return 1;
   }
-  
+
   return 0;
+}
+
+
+void printCsvResult(SQLite::Statement& query, std::ostream& stream, bool isFirstRow)
+{
+  stream << query.getColumn(0).getText()   << ","
+         << query.getColumn(1).getDouble() << ","
+         << query.getColumn(2).getDouble() << ","
+         << query.getColumn(3).getDouble() << ","
+         << query.getColumn(4).getDouble() << ","
+         << query.getColumn(5).getInt()    << ","
+         << query.getColumn(6).getInt()    << std::endl;
+}
+
+void printJsonResult(SQLite::Statement& query, std::ostream& stream, bool isFirstRow)
+{
+  if (!isFirstRow) {
+    stream << "," << std::endl;
+  }
+  
+  stream << "{ \"timestamp\": \"" << query.getColumn(0).getText() << "\" ,"
+         << "\"temp1\": "         << query.getColumn(1).getDouble()     << ","
+         << "\"temp2\": "         << query.getColumn(2).getDouble()     << ","
+         << "\"temp3\": "         << query.getColumn(3).getDouble()     << ","
+         << "\"temp4\": "         << query.getColumn(4).getDouble()     << ","
+         << "\"valve1\": "        << query.getColumn(5).getInt()        << ","
+         << "\"valve2\": "        << query.getColumn(6).getInt()        << "}";
 }
 
 parameterMap* parseURL(const std::string& url)
@@ -113,7 +160,7 @@ parameterMap* parseURL(const std::string& url)
   {
     auto parameter = new parameterMap();
     auto& current = queryList;
-      
+
     do
     {
       if (queryList->value != nullptr) {
@@ -122,15 +169,15 @@ parameterMap* parseURL(const std::string& url)
       else {
         (*parameter)[queryList->key] = std::string("");
       }
-      
+
       current = queryList->next;
     } while (current != nullptr);
-      
+
     uriFreeQueryListA(queryList);
     uriFreeUriMembersA(&uri);
-  
+
     return parameter;
   }
-  
+
   return new std::unordered_map<std::string, std::string>();
 }
