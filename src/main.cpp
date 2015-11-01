d7c296caaef173b55a140105f8e686ed142b0986
Tobias Tangemann <tobiastangemann@gmail.com>
2015-11-01 16:35:53 +0100

    Parse uri

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 #include <cstdlib>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
+
+#include <uriparser/Uri.h>
 #include <SQLiteCpp/SQLiteCpp.h>
 
 #define DB_PATH "/srv/http/data/vbus.sqlite"
+
+typedef std::unordered_map<std::string, std::string> parameterMap;
+
+parameterMap* parseURL(const std::string& url);
 
 void find_and_replace(std::string& source, std::string const& find, std::string const& replace)
 {
@@ -17,7 +24,8 @@ void find_and_replace(std::string& source, std::string const& find, std::string 
 }
 
 // See: http://stackoverflow.com/questions/15220861/how-can-i-set-the-comma-to-be-a-decimal-point
-class punct_facet: public std::numpunct<char> {
+class punct_facet: public std::numpunct<char>
+{
   protected: char do_decimal_point() const { return '.'; }
 };
 
@@ -26,22 +34,21 @@ int main(int argc, char const *argv[])
   // Set decimal seperator to '.'
   std::cout.imbue(std::locale(std::cout.getloc(), new punct_facet()));
   
+  // Print HTTP header
   std::cout << "Content-type: text/comma-separated-values\r\n"
             << "Cache-Control: no-cache, no-store, must-revalidate\r\n"
             << "Pragma: no-cache\r\n"
             << "Expires: 0\r\n"
             << "Access-Control-Allow-Origin: *\r\n\r\n";
 
-  try {
+  try
+  {
     SQLite::Database db(DB_PATH);
+    std::unique_ptr<parameterMap> requestParameter(parseURL(std::getenv("REQUEST_URI")));
 
-    std::string time_query = std::string("-1 hour");
-
-    char *cgi_query = std::getenv("QUERY_STRING");
-
-    if (cgi_query) {
-      time_query = std::string(cgi_query);
-      find_and_replace(time_query, "%20", " ");
+    if (requestParameter->count("timespan") == 0)
+    {
+      requestParameter->at("timespan") = "-1 hour";
     }
 
     SQLite::Statement query(db, "SELECT "
@@ -51,13 +58,12 @@ int main(int argc, char const *argv[])
                                 "WHERE time > (SELECT DATETIME('now', ?)) "
                                 "ORDER BY id");
 
-    query.bind(1, time_query);
-
-    //std::cout << time_query<< std::endl;
+    query.bind(1, (*requestParameter)["timespan"]);
 
     std::cout << "Datum,Ofen,Speicher-unten,Speicher-oben,Heizung,Ventil-Ofen,Ventil-Heizung" << std::endl;
 
-    while (query.executeStep()) {
+    while (query.executeStep())
+    {
       std::cout << query.getColumn(0).getText()   << ","
                 << query.getColumn(1).getDouble() << ","
                 << query.getColumn(2).getDouble() << ","
@@ -67,10 +73,53 @@ int main(int argc, char const *argv[])
                 << query.getColumn(6).getInt()    << std::endl;
     }
   }
-  catch (std::exception& e) {
+  catch (std::exception& e)
+  {
+      std::cerr << e.what() << std::endl;
       return 1;
   }
 
   /* code */
   return 0;
+}
+
+parameterMap* parseURL(const std::string& url)
+{
+  UriParserStateA state;
+  UriUriA uri;
+
+  state.uri = &uri;
+  if (uriParseUriA(&state, url.c_str()) != URI_SUCCESS)
+  {
+    uriFreeUriMembersA(&uri);
+    return new std::unordered_map<std::string, std::string>();
+  }
+
+  UriQueryListA* queryList;
+  int itemCount;
+
+  if (uriDissectQueryMallocA(&queryList, &itemCount, uri.query.first, uri.query.afterLast) == URI_SUCCESS)
+  {
+    auto parameter = new parameterMap();
+    auto& current = queryList;
+      
+    do
+    {
+      if (queryList->value != NULL) {
+        (*parameter)[queryList->key] = queryList->value;
+      }
+      else {
+        (*parameter)[queryList->key] = std::string("");
+      }
+      
+      current = queryList->next;
+    } while (current != NULL);
+      
+    uriFreeQueryListA(queryList);
+    uriFreeUriMembersA(&uri);
+  
+    return parameter;
+  }
+  
+  return new std::unordered_map<std::string, std::string>();
 }
